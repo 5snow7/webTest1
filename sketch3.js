4183553af9ab6d75c4a04a0732e68efb21f8f633
PhsTigers <mwomack@pcoe.k12.ca.us>
2018-09-19 18:28:00 -0700

    chged size of ellipse

--- sketch3.js
+++ sketch3.js
@@ -30,7 +30,8 @@ function draw() {
   col+=1;
   if(col>250){col=50;}
 text("test",50,50);
-  ellipse(50,50,100,200);
+  ellipse(50,50,300,200);
+  
 t.fd(200);t.lt(60);
 t.bk(300);
   fill(0, 0, 250);
